ce8a9431a5496587aded3717eba450022bf6b824
hojicya <hojicya01@gmail.com>
2024-07-17 12:00:50 +0900

    Add jQuery

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,10 @@
-console.log("test");
+import $ from "jquery";
+
+$(function () {
+  console.log("test");
+
+  // // ヘッダーの高さ分main下げる
+  // var headerHeight = $("#header").outerHeight();
+  // $("main").css("margin-top", headerHeight);
+  // $(".mv").css("height", `calc(100svh - ${headerHeight}px)`);
+});
