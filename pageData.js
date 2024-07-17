ad6b684a032c2384d3f1d68eb2315a6fa6fba444
hojicya <hojicya01@gmail.com>
2024-07-17 12:19:13 +0900

    Fix base layout

--- pageData.js
+++ pageData.js
@@ -1,16 +1,19 @@
 const baseData = {
   title: "タイトル",
   description: "トップページ",
+  url: "https://www.xxxx",
 };
 
 const pageData = {
   "/index.html": {
     title: baseData.title + "top",
     description: baseData.description,
+    url: baseData.url,
   },
   "/sample/index.html": {
     title: baseData.title + "sample",
     description: baseData.description,
+    url: baseData.url,
   },
 };
 
